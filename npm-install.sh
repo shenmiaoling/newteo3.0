9a051e649aafc7a4bdf7235951fbf73cdc93b47c
nodejs <shinchou@shinchou.local>
2017-01-16 17:34:09 +0800

    first commit

---
+++ npm-install.sh
@@ -0,0 +1,7 @@
+npm install react --save-dev
+npm install react-dom --save-dev
+npm install react-redux --save-dev
+npm install redux --save-dev
+
+npm install webpack --save-dev
+npm install babel-loader --save-dev
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path')
+module.exports = {
+  entry: './index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'build'),
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015','react']
+      }
+    }]
+  }
+}
